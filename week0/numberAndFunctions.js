3cc1a3e36ed2284d966b953346400649d8ea5336
Sashko Makarevich <oleksandr.makarevich@gmail.com>
2021-06-12 11:20:53 +0300

    first homework done

--- week0/numberAndFunctions.js
+++ week0/numberAndFunctions.js
@@ -1,30 +1,61 @@
 
 // 1. Write function to get random value in range from -100 to 100
-const random = (num1, num2) => {};
+const random = (min, max) => { return Math.random() * (max - min) + min};
 
 console.log('1: ', random(-100, 100))
 
 // 2. Write function to check is positive random number, 
+function isPositive(number) {
+    return number > 0;
+}
 
 console.log('2: ', isPositive(random(-100, 100)));
 // output should be boolean
 
 // 3. Write function, which create Array of random numbers. Array length = 6;
+function createRandomArray(length) {
+    const array = [];
+    for(let i = 0; i < length; i++) {
+        array.push(random(0, 100));
+    }
+    return array;
+}
+
+function getArraySum(array) {
+    return array.reduce((sum, element)=>{return sum += element});
+}
 // Result of function should be sum of array elements
 
-console.log('3:', getArraySum([]));
+const randArr = createRandomArray(6);
+console.log('3:', randArr, getArraySum(randArr));
 
 // 4. Write function to create random color
-
-console.log('4: ', getRandomColor(*, *, *));
+function getRandomColor() {
+    return {
+        red: Math.floor(random(0, 256)),
+        green: Math.floor(random(0, 256)), 
+        blue: Math.floor(random(0, 256)),
+        toString() {
+            return `rgb(${this.red}, ${this.green}, ${this.blue})`;
+        }
+    }
+}
+console.log('4: ', getRandomColor());
 // output rgb(*, *, *);
 
 // 5. Write function to find out if a number is a natural number or not
+function isNatural(...params) {
+    return !params.some((element) => !(Math.floor(element) === element && element > 0));
+}
 
-console.log('5: ', yourFuncName(10, 22, -23, 5.4));
+console.log('5: ', isNatural(10, 22, -23, 5.4));
+console.log(isNatural(10));
+console.log(isNatural(5.4));
 
 // 6. Create function isDivideBy(number, firstDivider, secondDivider). Check if an integer number is divisible by each out of two arguments
-
+function isDivisibleBy(number, firstDivider, secondDivider) {
+    return !(number % firstDivider) && !(number % secondDivider);
+}
 /**
  * (45, 1, 6)    ->  false
  * (45, 5, 15)   ->  true
@@ -33,4 +64,5 @@ console.log('5: ', yourFuncName(10, 22, -23, 5.4));
  * (-12, 2, -5)  ->  false
  */
 
-console.log('6: ', isDivideBy(*, *, *));
+console.log('6: ', isDivisibleBy(45, 1, 6));
+console.log(isDivisibleBy(45, 5, 15));
--- week0/string.js
+++ week0/string.js
@@ -1,50 +1,66 @@
 // instead of **** put your name and last name;
 
-const firstName = '****;'
-const lastName = '*****';
+const firstName = 'Sasha';
+const lastName = 'Makarevych';
 
 // 1. concat first and last name, to create full name e.g (Vitalii Danchul)
-const fullName = '';
+const fullName = firstName + " " + lastName;
 
 console.log('1: ', fullName);
 
 // 2. log fullName third character. Please, use more than one solution
-console.log('2.1: ', );
-console.log('2.2: ', );
+console.log('2.1: ', fullName[2]);
+console.log('2.2: ', fullName.charAt(2));
+console.log('2.3', fullName.slice(2, 3));
 
 
 // 3. replace all vowels in fullName with number
 const number = 10;
-const replaced = fullname.****(*****);
+const replaced = fullName.replace(/[aeiou]/g, number);
 
-console.log('3: ', );
+console.log('3: ', replaced);
 
 
 // 4. check if replaced string on position 8 ends with number 10
 
-console.log('4: ', );
+console.log('4: ', replaced.slice(8, 10) == "10" ); // (??)
 
 // 5. log part of fullName from 3 to 7 character
 
-console.log('5: ', );
+console.log('5: ', fullName.slice(3, 8));
 
 // 6. Divide replaced string by number 10
 
-console.log('6:', )
+console.log('6:', replaced.split("10"))
 
 // 7. Write function capitalize(), to capitalize first letter of string 
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
 const lowerCased = 'new york';
 
 console.log('7:', capitalize(lowerCased));
 
 // 8. Write insert function, that will accept main string, string to insert and position to insert
 // Change **** with your name 
+function insert(string, substr, position) {
+    return string.slice(0, position) + substr + string.slice(position);
+}
 
-const mainString = 'Hello, my name is ****, I want to learn Node.js';
-console.log('8:' , insert(mainString, fullName, *****))
+
+const mainString = 'Hello, my name is , I want to learn Node.js';
+console.log('8:' , insert(mainString, fullName, 18));
 
 // 9. Write your own reverse() function. (do not use .reverse())
 
-console.log('9', reverse('my random string'));
+function reverse(str) {
+    let result = "";
+    for (i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+    return result;
+}
+
+console.log('9:', reverse('my random string'));
 
 
